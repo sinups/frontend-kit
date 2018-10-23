863e52aa43b06f19c8abf055dc286defd86593e9
Andrei Cojocari <webtheory@mail.ru>
2018-10-23 13:13:29 +0300

    png sprite

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
 @@include('partials/jquery-3.3.1.min.js');
 @@include('partials/slick.js');
 @@include('partials/main.js');
+
+
+//Console log test
+console.log("%cFrontend-kit",'font-size: 46px; color: red; ');
